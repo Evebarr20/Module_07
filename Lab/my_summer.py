061c2e208d7658b08a4c1c23b060574a0293c164
Evelyn Barragan <barragan2020eb@outlook.com>
2026-02-18 19:53:19 -0800

    Upload lab files

---
+++ Lab/my_summer.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 29 11:07:40 2025
+
+@author: SheetsH
+"""
+
+
+
+def sum(x):
+    y=0
+    for i in range(len(x)):
+        y=y+x[i]
+    return(y)
+
---
+++ Lab/profiler_homework.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 29 10:59:28 2025
+
+@author: SheetsH
+"""
+
+import numpy as np
+
+import my_summer as summer
+
+#set the matrix sizes
+
+M = 10**7
+
+
+# set the seed on the random number generator
+# so we get repeatable results
+
+np.random.seed(42)
+
+# generate random matrices to work with
+
+A = np.random.random((M))
+
+x=summer.sum(A)
+
+print(x)
